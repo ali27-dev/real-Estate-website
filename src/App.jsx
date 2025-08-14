192e68e3d032c5c92361cb000a7d1d75fd933120
ali27-dev <khanali3229@gmail.com>
2025-08-14 22:43:49 +0500

    after the state section i am added compaines section and move to next section Residences i will bulid as soon as possible

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import "./App.css";
+import Companies from "./components/Companies/Companies.jsx";
+import Residences from "./components/Residences/Residences.jsx";
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
+      <Residences />
     </div>
   );
 }
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,24 @@
+import "./Companies.css";
+export default function Companies() {
+  return (
+    <div>
+      <section className="c-wrapper">
+        <div className="c-container flex-center">
+          <img
+            src="./prologis.png"
+            alt="the image is about pro-logis company"
+          />
+          <img
+            src="./tower.png"
+            alt="the image is about American-Tower company"
+          />
+          <img
+            src="./realty.png"
+            alt="the image is about digital-realty company"
+          />
+          <img src="./equinix.png" alt="the image is about Equinix company" />
+        </div>
+      </section>
+    </div>
+  );
+}
