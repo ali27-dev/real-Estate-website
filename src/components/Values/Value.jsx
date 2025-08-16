7f8b11379385c3c162d1a865efce8a4c1c5d3108
ali27-dev <khanali3229@gmail.com>
2025-08-16 13:17:04 +0500

    i am bulid the Value section and i use Accordion library to create expended slide but at last i am facing error in the bulid of AccordionItemState component the functionality are working but the error is still in the console i will be fix this soon

--- src/components/Values/Value.jsx
+++ src/components/Values/Value.jsx
@@ -9,6 +9,9 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import "./Value.css";
+import Data from "../../utils/accordion";
+import { useState } from "react";
+
 export default function Value() {
   return (
     <section className="v-wrapper">
@@ -20,10 +23,55 @@ export default function Value() {
           </div>
         </div>
         {/* right */}
-        <div className="v-right flex-col-center">
-          <span>A</span>
-          <span>B</span>
-          <span>C</span>
+        <div className="v-right flex-col-start">
+          <h3 className="orange-text">Our Value</h3>
+          <h2 className="primary-text">Value We Give to You</h2>
+          <p className="secondary-text">
+            We always ready to help by providijng the best services for you.
+            <br />
+            We beleive a good blace to live can make your life better
+          </p>
+          {/* Accordion-section */}
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {Data.map((item, i) => {
+              let [classState, setClassState] = useState(null);
+              return (
+                <AccordionItem
+                  className={`accordion-item ${classState}`}
+                  key={i}
+                  uuid={i}
+                >
+                  {/* Accordion-heading */}
+                  <AccordionItemHeading>
+                    <AccordionItemButton className="accordion-btn flex-center">
+                      {/* Accordion-Item-State */}
+                      <AccordionItemState>
+                        {({ expanded }) =>
+                          expanded
+                            ? setClassState("expanded")
+                            : setClassState("")
+                        }
+                      </AccordionItemState>
+
+                      <div className="flex-center icon">{item.icon}</div>
+                      <h4 className="primary-text">{item.heading}</h4>
+                      <div className="flex-center icon">
+                        <MdOutlineArrowDropDown />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  {/* Accordion-Panel */}
+                  <AccordionItemPanel>
+                    <p className="secondary-text">{item.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
         </div>
       </div>
     </section>
